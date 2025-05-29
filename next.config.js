d2d280b92b708ab1a12ada8fecadd5ce688a06a3
张施泽 <1194067216@qq.com>
2025-05-29 23:37:03 +0800

    replace path

--- next.config.js
+++ next.config.js
@@ -29,6 +29,7 @@ const nextConfig = {
   reactStrictMode: false,
   output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '/Tiktok-Desensitized' : '',
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
